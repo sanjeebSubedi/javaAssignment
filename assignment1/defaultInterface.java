d6758a68b80fe2edbf2e44fa582f1e4a8c6aa5d9
Sanjeeb Subedi <51266822+sanjeebSubedi@users.noreply.github.com>
2020-02-11 17:04:23 +0545

    Update defaultInterface.java

--- assignment1/defaultInterface.java
+++ assignment1/defaultInterface.java
@@ -1,16 +1,24 @@
+
 interface my_interface
     {
-        void  greet();
-        static void welcome()
+         default void welcome()
         {
             System.out.println("Welcome to Nepal");
         }
     }
 
-class my_class
+
+class my_class implements my_interface
     {
+        public void sample()
+        {
+            System.out.println("Have a nice journey!");
+        }
     public static void main(String[] args)
         {
-                my_interface.welcome();
+            my_class example = new my_class();
+            example.welcome();
+            example.sample();
+
         }
     }
