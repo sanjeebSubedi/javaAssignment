07f47c86a6215f00905d96435549de9e712be004
unknown <sanjeebsubedi4@gmail.com>
2020-02-16 21:08:34 +0545

    first commit

---
+++ assignment3/arrrayInJava..java
@@ -0,0 +1,11 @@
+public class arrayDemo
+{
+   public static void main(String[] args) 
+    {
+    String myArray[] = {"apple", "ball", "cat", "dog", "elephant"};
+    for (int i = 0; i < 5; i++) 
+       {
+        System.out.println(myArray[i]);
+       }
+    }
+}
---
+++ assignment3/enhancedForLoop.java
@@ -0,0 +1,11 @@
+public class enhancedLoop 
+{
+    public static void main(String[] args) 
+    {
+        String myArray[] = {"apple", "ball", "cat", "dog", "elephant"};
+        for (String i : myArray) 
+	{
+            System.out.println(i);
+        }
+    }
+}
