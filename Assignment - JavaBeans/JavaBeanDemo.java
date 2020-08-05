fba060f3e5de2a8fbd5f0739805b6484cd5a1230
Sanjeeb Subedi <51266822+sanjeebSubedi@users.noreply.github.com>
2020-08-05 21:57:40 +0545

    Create JavaBeanDemo.java

---
+++ Assignment - JavaBeans/JavaBeanDemo.java
@@ -0,0 +1,38 @@
+import java.io.Serializable;
+
+public class JavaBeanDemo implements Serializable{
+	private int id;
+	private String name, address;
+	private float salary;
+	
+	public JavaBeanDemo() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	
+	
+}
